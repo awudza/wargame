e3c97615da9f944f0413bdf1004b5f602ec8e666
awudza1u <awudza1u@etu.univ-lorraine.fr>
2023-05-20 12:00:46 +0200

    ajout de attributs de la classe Terrain

--- src/projet/modeles/Terrain.java
+++ src/projet/modeles/Terrain.java
@@ -1,5 +1,47 @@
 package projet.modeles;
 
 public class Terrain {
+    private int id;
+    private int largeur;
+    private int hauteur;
 
+    public Terrain(){
+
+    }
+
+    /**
+     *
+     * @return largeur du terrain
+     */
+
+    public int getLargeur() {
+        return largeur;
+    }
+
+    /**
+     *
+     * @return hauteur du terrain
+     */
+
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    /**
+     *
+     * @param hauteur
+     */
+
+    public void setHauteur(int hauteur) {
+        this.hauteur = hauteur;
+    }
+
+    /**
+     *
+     * @param largeur
+     */
+
+    public void setLargeur(int largeur) {
+        this.largeur = largeur;
+    }
 }
