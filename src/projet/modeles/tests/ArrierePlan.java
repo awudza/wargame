b4b4a231f334bb712cc76804e35b1b17d6090927
awudza1u <awudza1u@etu.univ-lorraine.fr>
2023-05-21 18:26:31 +0200

    grille hexagonale

---
+++ src/projet/modeles/tests/ArrierePlan.java
@@ -0,0 +1,11 @@
+package projet.modeles.tests;
+
+import javax.swing.*;
+
+public class ArrierePlan extends JFrame {
+    private JPanel mainPanel;
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+}
