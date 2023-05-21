a9ecc1c4a50b16ed81754a67fa256629acc7f66e
FOLLY Yao Hippolyte <follyyaohippolyte@gmail.com>
2023-05-22 01:55:30 +0200

    Change hexagone border on mouse moved

---
+++ src/projet/view/Background.java
@@ -0,0 +1,23 @@
+package projet.view;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Background extends JLayeredPane {
+    private Image backgroundImage;
+    private ImageIcon ico;
+    public Background() {
+        super();
+        ico=new ImageIcon(getClass().getResource("../../resources/map.png"));
+        this.backgroundImage=this.ico.getImage();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics g2=(Graphics2D) g;
+        g2.drawImage(backgroundImage, 0, 0, this);
+    }
+
+}
